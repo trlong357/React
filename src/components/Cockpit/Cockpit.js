c38a183287e22aea8bfdc0ed571a89a40eb0d3ff
Long Tran <trlong357@gmail.com>
2021-02-11 00:25:24 +0700

    94. Controlling the useEffect() Behavior

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,13 +1,23 @@
 import React, { useEffect } from "react";
 // useEffect combines the functionality or the use cases you can
 // cover of all these class-based lifcycle hooks
+
+// Effects Hooks are equivalent to componentDidMount(),
+// componentDidUpdate(), and componentWillUnmount() lifecycle methods.
 import classes from "./Cockpit.module.css";
 
 const Cockpit = (props) => {
-  useEffect(() => {
-    console.log("[Cockpit.js] useEffect");
-    // can send HTTP requets or ...
-  });
+  useEffect(
+    () => {
+      console.log("[Cockpit.js] useEffect");
+      // can send HTTP requets or ...
+      setTimeout(() => {
+        alert("learn learn learn!");
+      }, 1000);
+    },
+    // [] // --> this alert one first time
+    [props.show] //--> alert when u drop-down toggle
+  );
 
   const classesAssign = [];
 
